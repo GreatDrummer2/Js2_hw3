9d4f9a744de5a1b87c4b0bcc7d47de3e6b597910
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-22 00:17:27 +0200

    good version

--- part5.js
+++ part5.js
@@ -2,7 +2,15 @@ function correctEmail(){
   var elem = document.getElementById('form_1');
   elem.classList.remove(elem.classList[0]); 
   var str = elem.value;
-  var emailIndex = str.match(/@gmail.com|@mail.ru|@yahoo.com|@yandex.ru|@ukr.net/ig);
+  var arr = ['gmail.com','mail.ru','yahoo.com','yandex.ru','ukr.net'];
+  var emailIndex;
+  arr.forEach(function(item){
+    var re = new RegExp('@' + item, 'gi');
+    var brand= str.match(re);
+    if(brand != null){
+      emailIndex = brand;
+    }
+  }); 
   var nickName = str.match(/[0-9a-z]{3,}(?=@)/ig);
   var newStr = nickName + emailIndex;
   var clazName = newStr == str ? 'green' : 'red';
