cda7853b1839c85683f5fbd1dac02e52d603f131
GreatDrummer2 <phillippochta@yahoo.com>
2017-02-22 15:07:59 +0200

    all done

--- part5.js
+++ part5.js
@@ -6,13 +6,30 @@ function correctEmail(){
   var emailIndex;
   arr.forEach(function(item){
     var re = new RegExp('@' + item, 'gi');
-    var brand= str.match(re);
+    var brand = str.match(re);
     if(brand != null){
       emailIndex = brand;
     }
   }); 
   var nickName = str.match(/[0-9a-z]{3,}(?=@)/ig);
   var newStr = nickName + emailIndex;
+  var clazName = newStr == str ? 'green' : 'red';
+  elem.classList.add(clazName);
+}
+
+function correctPI(){
+  var elem = document.getElementById('form_2');
+  elem.classList.remove(elem.classList[0]); 
+  var str = elem.value;
+  var newStr = str.match(/\d{4} \d{6}/g);
+  var clazName = newStr == str ? 'green' : 'red';
+  elem.classList.add(clazName);
+}
+function correctPN(){
+  var elem = document.getElementById('form_3');
+  elem.classList.remove(elem.classList[0]); 
+  var str = elem.value;
+  var newStr = str.match(/\+\d\(\d{3}\)\-\d{3}\-\d{2}\-\d{2}/g);
   var clazName = newStr == str ? 'green' : 'red';
   elem.classList.add(clazName);
 }
